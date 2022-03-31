39fa31103e4a9809dc8b9d7aac535db9ac0bc144
Handrei Fontana <95292655+HandreiFontana@users.noreply.github.com>
2022-03-31 17:31:27 -0300

    feat: docker installed and database container created

---
+++ src/typeorm/index.ts
@@ -0,0 +1,13 @@
+import { Connection, createConnection, getConnectionOptions } from "typeorm";
+
+export default async (host = "localhost"): Promise<Connection> => { // host = localhost ??
+    const defaultOptions = await getConnectionOptions();
+
+    return createConnection(
+        Object.assign(defaultOptions, {
+            host,
+            database: "linkin_db"
+        })
+    );
+
+};
