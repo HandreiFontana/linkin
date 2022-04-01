b35069d7804f02a8f942c363eb463e0bcf9dcf95
Handrei Fontana <95292655+HandreiFontana@users.noreply.github.com>
2022-04-01 14:49:58 -0300

    feat(link entitie): preparing to create a link table at the database; entities and columns prepared

--- src/modules/links/entities/link.ts
+++ src/modules/links/entities/link.ts
@@ -1,13 +1,30 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm"
 import { v4 as uuidV4 } from 'uuid';
 
+@Entity("links")
 class Link {
+    @PrimaryColumn()
     id?: string;
+
+    @Column()
     title: string;
+
+    @Column()
     description: string;
+
+    @Column()
     url: string;
+
+    @Column()
     category: string;
+
+    @Column()
     created_by: string;
+
+    @Column()
     isPrivate?: boolean;
+
+    @CreateDateColumn()
     created_at: Date;
 
     constructor() {
