5e2c720be6cec6d3f9732263509895389f0f3ef1
Handrei Fontana <95292655+HandreiFontana@users.noreply.github.com>
2022-04-05 18:28:32 -0300

    WIP(RefreshToken): Function created; refresh token and account havent been founded

---
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
@@ -0,0 +1,79 @@
+import { sign, verify } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import { IDateProvider } from "../../../../shared/container/providers/DateProvider/IDateProvider";
+import { IAccountsTokensRepository } from "../../repositories/IAccountsTokensRepository";
+
+import auth from "../../../../config/auth";
+import { AppError } from "../../../../shared/errors/AppErrors";
+
+
+interface IPayload {
+    sub: string;
+    email: string;
+}
+
+interface ITokenResponse {
+    token: string;
+    refresh_token: string;
+}
+
+@injectable()
+class RefreshTokenUseCase {
+    constructor(
+        @inject("AccountsTokensRepository")
+        private accountsTokensRepository: IAccountsTokensRepository,
+        @inject("DayjsDateProvider")
+        private dateProvider: IDateProvider
+    ) { }
+
+    async execute(token: string): Promise<ITokenResponse> {
+        const { email, sub } = verify(
+            token,
+            auth.secret_refresh_token,
+        ) as IPayload;
+
+        const account_id = sub;
+
+        const accountToken = await this.accountsTokensRepository
+            .findByAccountIdAndRefreshToken(
+                account_id,
+                token,
+            );
+
+        console.log(accountToken)
+
+        if (!accountToken) {
+            throw new AppError("Refresh token does not exists!")
+        }
+
+        await this.accountsTokensRepository.deleteById(accountToken.id);
+
+        const refresh_token = sign({ email }, auth.secret_refresh_token, {
+            subject: sub,
+            expiresIn: auth.secret_refresh_token,
+        });
+
+        const expires_date = this.dateProvider.addDays(
+            auth.expires_refresh_token_days
+        )
+
+        await this.accountsTokensRepository.create({
+            expires_date,
+            refresh_token,
+            account_id
+        })
+
+        const newToken = sign({}, auth.secret_token, {
+            subject: account_id,
+            expiresIn: auth.expires_in_token,
+        });
+
+        return {
+            refresh_token,
+            token: newToken
+        }
+    }
+}
+
+export { RefreshTokenUseCase };
--- src/shared/infra/http/routes/authenticate.routes.ts
+++ src/shared/infra/http/routes/authenticate.routes.ts
@@ -1,12 +1,17 @@
 import { Router } from 'express';
+
 import { AuthenticateAccountController } from '../../../../modules/accounts/useCases/authenticateAccount/AuthenticateAccountController';
+import { RefreshTokenController } from '../../../../modules/accounts/useCases/refreshToken/RefreshTokenController';
 
 
 const authenticateRoutes = Router();
 
 const authenticateAccountController = new AuthenticateAccountController();
+const refreshTokenController = new RefreshTokenController();
 
 authenticateRoutes.post("/sessions", authenticateAccountController.handle);
+authenticateRoutes.post("/refresh-token", refreshTokenController.handle);
+
 
 
 export { authenticateRoutes }
