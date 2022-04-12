cc78f3aab978a36022dc08aa22363a4d18669900
Handrei Fontana <95292655+HandreiFontana@users.noreply.github.com>
2022-04-11 21:33:06 -0300

    feat(migration): Create Category migration created

---
+++ src/shared/infra/typeorm/migrations/1649721542041-CreateCategory.ts
@@ -0,0 +1,47 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateCategory1649721542041 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "categories",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true,
+                    },
+                    {
+                        name: "name",
+                        type: "varchar",
+                    },
+                    {
+                        name: "account_id",
+                        type: "uuid",
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()",
+                    }
+                ],
+                foreignKeys: [
+                    {
+                        name: "FKCreateCategory",
+                        referencedTableName: "accounts",
+                        referencedColumnNames: ["id"],
+                        columnNames: ["account_id"],
+                        onDelete: "CASCADE",
+                        onUpdate: "CASCADE",
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("categories")
+    }
+
+}
